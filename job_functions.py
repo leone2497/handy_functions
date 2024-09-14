46912c2de57ad29db80c4207aa9e5a06cd634719
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-14 19:20:33 +0200

    Update job_functions.py

--- job_functions.py
+++ job_functions.py
@@ -9,7 +9,7 @@ st.title("Handy functions")
 st.sidebar.title("Functions")
 Functions = st.sidebar.selectbox("Choose a function", ["Extract text from image"])
 
-if action == "Extract text from image":
+if Functions == "Extract text from image":
   st.subheader("Extract text from image")
   uploaded_file = st.file_uploader("Upload image.", type=["jpg", "png", "jpeg"])
   image = Image.open(uploaded_file)
