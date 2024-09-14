756df78bb94c19b3ba3543c67515374f8c4b0c81
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-14 19:49:22 +0200

    Update job_functions.py

--- job_functions.py
+++ job_functions.py
@@ -7,11 +7,11 @@ from PIL import Image
 
 st.title("Handy Functions")
 st.sidebar.title("Functions")
-function_choice = st.sidebar.selectbox("Choose a function", ["Extract text from image"])
+function_choice = st.sidebar.selectbox("Choose a function", ["Extract text from image","Join files"])
 
 if function_choice == "Extract text from image":
     st.subheader("Extract text from image")
-    uploaded_file = st.file_uploader("Upload image.", type=["jpg", "png", "jpeg"])
+    uploaded_file = st.file_uploader("Upload image", type=["jpg", "png", "jpeg"])
     
     if uploaded_file is not None:
         try:
@@ -28,6 +28,25 @@ if function_choice == "Extract text from image":
             st.error(f"Error processing the image: {e}")
     else:
         st.info("Please upload an image file.")
+else function_choice == "Join files":
+    st.subheader("Join files")
+    uploaded_file = st.file_uploader("Upload files", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        try:
+            image = Image.open(uploaded_file)
+            img_np = np.array(image)
+            
+            reader = easyocr.Reader(['en', 'it'], gpu=False)
+            result = reader.readtext(img_np)
+            
+            st.subheader("Extracted Text:")
+            for _, text, _ in result:
+                st.text(text)
+        except Exception as e:
+            st.error(f"Error processing the image: {e}")
+    else:
+        st.info("Please upload an file.")
+
     
 
 
