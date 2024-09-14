a732f70e48ffd09db7bf18e203fb63fbb4aeee16
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-14 18:51:43 +0200

    Update job_functions.py

--- job_functions.py
+++ job_functions.py
@@ -5,8 +5,18 @@ import easyocr
 import numpy as np
 from glob import glob 
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-image = Image.open(uploaded_file)
-img_np = np.array(image)
-reader = easyocr.Reader(['en', 'it'], gpu=False)
-result = reader.readtext(img_np)
+st.title("Handy functions")
+st.sidebar.title("Functions")
+Functions = st.sidebar.selectbox("Choose a function", ["Extract text from image"])
+
+if action == "Extract text from image":
+  st.subheader("Extract text from image")
+  uploaded_file = st.file_uploader("Upload image.", type=["jpg", "png", "jpeg"])
+  image = Image.open(uploaded_file)
+  img_np = np.array(image)
+  reader = easyocr.Reader(['en', 'it'], gpu=False)
+  result = reader.readtext(img_np)
+    
+
+
+
