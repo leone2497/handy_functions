9805cbb56133f263d039c099165c798e6e7a1d0f
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-14 18:46:59 +0200

    Update job_functions.py

--- job_functions.py
+++ job_functions.py
@@ -2,6 +2,11 @@ import pandas as pd
 import matplotlib as mt
 import streamlit as st
 import easyocr
+import numpy as np
+from glob import glob 
 
-
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+image = Image.open(uploaded_file)
+img_np = np.array(image)
 reader = easyocr.Reader(['en', 'it'], gpu=False)
+result = reader.readtext(img_np)
